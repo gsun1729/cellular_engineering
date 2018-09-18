116309dd880fb2879c03ebb9d692892fa42475a5
gsun1729 <gsun1729@gmail.com>
2018-09-18 12:17:10 -0400

    working WT portion

--- hw1/CellEng_HW1_2018Template.m
+++ hw1/CellEng_HW1_2018Template.m
@@ -2,16 +2,12 @@
 %Author:  Gordon Sun
 %Date:    20180915
 %
-%This program uses nonlinear least squares on wild-type and mutant protein/ligand interaction data to... 
+%This program uses nonlinear least squares on wild-type and mutant protein/ligand interaction data to...
 %Assumptions:  Assuming steady state.
 %This file outputs 4 graphs
 
 function myFlag = CellEng_HW1_2018Template()
-
-clear all;
-close all;
-myFlag=0;
-
+clear; close all; clc;
 %Wild-Type Data.  [L] (nM) vs signal
 
 L0   = [1 2 4 8 10 20 40 80 100 200 400 800 1600 3200 6400 1e4];   % Given ligand concentration (independent variable)
@@ -31,7 +27,7 @@ WT   = [0.0178
     0.9892
     0.9846
     0.9965
-    ];   % WT Signal (dependent variable)
+    ]';   % WT Signal (dependent variable)
 MUT  = [0.0312
     0.0786
     0.1454
@@ -48,47 +44,45 @@ MUT  = [0.0312
     0.7384
     0.7198
     0.6828
-    ];   % Mut Signal (dependent variable)
+    ]';   % Mut Signal (dependent variable)
 
-beta0 = [0, 0];      % Parameter 1:  Initial Guesses: Constant of Proportionality between fraction saturated and signal 
-                    % Parameter 2:  Initial Guesses: Kd [=] nM 
-                                 
-[WT_beta, WT_res] = nlinfit(L0, WT, @, beta0),    % WT_beta = [proportionality constant, Kd] , ** This needs to be filled in to run **
+beta0 = [0, 0];      % Parameter 1:  Initial Guesses: Constant of Proportionality between fraction saturated and signal
+% Parameter 2:  Initial Guesses: Kd [=] nM
 
- %Plots experimental wild-type data
- figure;
-  semilogx( );  % what you are plotting
-  xlabel(' ', 'FontSize', 16);
-  ylabel(' ', 'FontSize', 16);
-  title(' ', 'FontSize', 18);
-  axis([ ]);
+[WT_beta, WT_res] = nlinfit(L0, WT, @binding, beta0);    % WT_beta = [proportionality constant, Kd] , ** This needs to be filled in to run **
+
+%Plots experimental wild-type data
+figure;
+semilogx(L0, WT);  % what you are plotting
+xlabel('Ligand Concentration (nM)', 'FontSize', 16);
+ylabel('WT Signal', 'FontSize', 16);
+title('WT Protein Signal vs. Ligand Concentration', 'FontSize', 18);
+axis([ ]);
 
 %Plots fit of wild-type data
- hold on;
-  p =  ; % this is your array of ligand points to use for fitting
-  WT_fit =  % this will be a function of WT_beta(1) WT_beta(2), and p
-  semilogx( ); % what you are plotting
-  legend( );
- hold off;
-  
- %Plots Residuals
- figure;
-  semilogx( );  % what you are plotting
-  xlabel(' ', 'FontSize', 16);
-  ylabel(' ', 'FontSize', 16);
-  title(' ', 'FontSize', 18);
-  axis([  ]);
-  
-%Repeat with Mutant Data Below.  [L] (nM) vs Mutant Signal
-  
+hold on;
+p =  L0; % this is your array of ligand points to use for fitting
+WT_fit = WT_beta(1) * p ./ (p + WT_beta(2)); % this will be a function of WT_beta(1) WT_beta(2), and p
+semilogx(L0, WT_fit); % what you are plotting
+legend('Raw WT data', 'WT Fit');
+hold off;
 
-  myFlag=1;   % Program finished
-return; 0
+%Plots Residuals
+figure;
+semilogx(L0, WT_res);  % what you are plotting
+xlabel('Ligand Concentration (nM)', 'FontSize', 16);
+ylabel('Residuals', 'FontSize', 16);
+title('WT Residuals', 'FontSize', 18);
+axis([  ]);
+
+%Repeat with Mutant Data Below.  [L] (nM) vs Mutant Signal
+
+return; 
 
 
 %------------------Function that describes the curve that data points are fit to--------------
 function signal = binding(beta, L)
-      Var1   = beta(1);
-      Var2   = beta(2);
-      signal  =  ; % Will be a function of L and what you name as Variables (Var1 and Var2), equation from class
+Var1   = beta(1);
+Var2   = beta(2);
+signal  =  Var1 * L ./ (L + Var2); % Will be a function of L and what you name as Variables (Var1 and Var2), equation from class
 return;
